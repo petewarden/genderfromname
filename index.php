68d9f17c9e4f195f87811667b4c78bca10e9ab9b
Pete Warden <pete@petewarden.com>
2009-12-31 17:53:32 -0700

    Added index.php example page

---
+++ index.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * Test page for the GenderFromName module. If there's a 'name' URL parameter, it will use the module
+ * to guess the gender based on the first word in the name
+ * By Pete Warden <pete@petewarden.com>
+ * See http://petewarden.typepad.com/ for more details
+ *
+ */
+
+require_once ('genderfromname.php');
+
+if (isset($_REQUEST['name'])) {
+	$name = urldecode($_REQUEST['name']);
+} else {
+	$name = '';
+}
+
+?>
+<html>
+<head>
+<title>Test page for the GenderFromName module</title>
+</head>
+<body>
+<div style="padding:20px;">
+<center>
+<form method="GET" action="index.php">
+Name: <input type="text" size="40" name="name" value="<?=$name?>"/>
+</form>
+</center>
+</div>
+<div>
+<center>
+<?php
+if ($name!='') {
+
+    $DEBUG = 1;
+
+    $strictness = 9; // Set this to 1 or 2 for more restrictive matching
+
+    $nameparts = explode(' ', $name);
+    $firstname = $nameparts[0];
+    $gender = gender($firstname, $strictness);
+
+    if (isset($gender))
+    {
+        if ($gender==='f')
+            $gendername = 'female';
+        else
+            $gendername = 'male';
+    
+        print "GenderFromName guesses that '$name' is $gendername"; 
+    }
+    else
+    {
+        print "GenderFromName couldn't make a good guess what $name's gender is";     
+    }
+
+}
+?>
+</center>
+</div>
+</body>
+</html>
